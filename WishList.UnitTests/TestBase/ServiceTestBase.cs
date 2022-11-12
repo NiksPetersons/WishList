6534d062cc00b50fa005846838ccc56d1a2b68a4
Niks Pētersons <verbatim1235@gmail.com>
2022-11-12 15:42:58 +0200

    Regular and in-memory tests

---
+++ WishList.UnitTests/TestBase/ServiceTestBase.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.EntityFrameworkCore;
+using WishList.DataBase;
+using WishList.Services;
+
+namespace WishList.UnitTests.TestBase;
+
+public class ServiceTestBase : IDisposable
+{
+    protected readonly WishListDbContext _context;
+
+    public ServiceTestBase()
+    {
+        var options = new DbContextOptionsBuilder<WishListDbContext>()
+            .UseInMemoryDatabase(databaseName: Guid.NewGuid().ToString())
+            .Options;
+        _context = new WishListDbContext(options);
+
+        _context.Database.EnsureCreated();
+    }
+
+    public void Dispose()
+    {
+        _context.Database.EnsureDeleted();
+        _context.Dispose();
+    }
+}
---
+++ WishList.UnitTests/ValidationTests.cs
@@ -0,0 +1,52 @@
+﻿using FluentAssertions;
+using WishList.Core.Validations;
+using Wishlist.Web.RequestModels;
+
+namespace WishList.UnitTests;
+
+public class ValidationTests
+{
+    private readonly IWishValidator _nameValidator;
+    public ValidationTests()
+    {
+        _nameValidator = new WishNameValidator();
+    }
+
+    [Fact]
+    public void IsValid_GiveCorrectData_ShouldBeTrue()
+    {
+        //Arrange
+        var wishRequest = new WishRequest
+        {
+            Name = "Test wish"
+        };
+
+        //Act
+        var validation = _nameValidator.IsValid(wishRequest);
+
+        //Assert
+        validation.Should().BeTrue();
+    }
+
+    [Fact]
+    public void IsValid_GiveEmptyOrNullName_ShouldBeFalse()
+    {
+        //Arrange
+        var emptyWishRequest = new WishRequest
+        {
+            Name = ""
+        };
+        var nullWishRequest = new WishRequest
+        {
+            Name = null
+        };
+
+        //Act
+        var emptyValidation = _nameValidator.IsValid(emptyWishRequest);
+        var nullValidation = _nameValidator.IsValid(nullWishRequest);
+
+        //Assert
+        emptyValidation.Should().BeFalse();
+        nullValidation.Should().BeFalse();
+    }
+}
